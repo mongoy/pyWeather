6f408c4f8b89400cf499a2a8d6300fc669f77666
mongoy <mongoy@rambler.ru>
2019-04-26 08:58:37 +0900

    Create app

--- weather/views.py
+++ weather/views.py
@@ -2,19 +2,32 @@ import requests
 from django.shortcuts import render
 
 
-API_key = 'b4ade3e9f47c260f170c8ee12da73bb0' # ключ с сайта https://openweathermap.org
-
 def index(request):
-    url = 'https://api.openweathermap.org/data/2.5/forecast?q={},RU&unit=metric&appid=' + API_key
+    API_ID = 'b4ade3e9f47c260f170c8ee12da73bb0'  # ключ с сайта https://openweathermap.org
     city = 'Chita'
-    res = requests.get(url.format(city)).json()
-    #выделяем необходимую информацию из url
-    city_info = {
-        'city': city,
-        #'temp': str(res["main"]["temp"]),
-        #'info': res['weather']['icon']
-    }
+    city_id = 0
+    url = 'https://api.openweathermap.org/data/2.5/forecast?q={},RU&unit=metric&appid='
 
-    context = {'info': city_info}
-    #print(res.text)
-    return render(request, 'weather/index.html', context)
+    try:
+        res = requests.get(url, params={'q': city, 'type': 'like', 'APPID': API_ID}) # запрос на сервер
+        data = res.json() # получение JSON файла
+        print(data)
+        city_info = ["{}  ({})".format(d["name"], d["sys"]["country"]) for d in data["list"]] #получение информации и SON файла
+        print("city :", city_info)
+    except Exception as e:
+        print("Exception (find):", e)
+
+    # url = 'https://api.openweathermap.org/data/2.5/forecast?q={},RU&unit=metric&appid=' + API_key
+    #
+    # res = requests.get(url.format(city)).json()
+    # #выделяем необходимую информацию из url
+    # city_info = {
+    #     'city': city,
+    #     #'temp': str(res["main"]["temp"]),
+    #     #'info': res['weather']['icon']
+    # }
+    #
+    # context = {'info': city_info}
+    # #print(res.text)
+
+    return render(request, 'weather/index.html')
