ccce4d669ea3757749ee710de6e291f713d20535
mongoy <mongoy@rambler.ru>
2019-04-28 20:21:54 +0900

    edit views

--- weather/views.py
+++ weather/views.py
@@ -1,33 +1,30 @@
+import time
 import requests
 from django.shortcuts import render
 
 
 def index(request):
     API_ID = 'b4ade3e9f47c260f170c8ee12da73bb0'  # ключ с сайта https://openweathermap.org
-    city = 'Chita'
-    city_id = 0
-    url = 'https://api.openweathermap.org/data/2.5/forecast?q={},RU&unit=metric&appid='
+    city = 'Чита'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={},ru&units=metric&APPID=' + API_ID
 
     try:
-        res = requests.get(url, params={'q': city, 'type': 'like', 'APPID': API_ID}) # запрос на сервер
-        data = res.json() # получение JSON файла
-        print(data)
-        city_info = ["{}  ({})".format(d["name"], d["sys"]["country"]) for d in data["list"]] #получение информации и SON файла
+
+        res = requests.get(url.format(city)).json()# запрос на сервер получение JSON файла
+        # выделяем необходимую информацию из url
+        city_info = {
+            'city': city, #название города
+            'dt': time.strftime("%Y-%m-%d %H:%M:%S",time.gmtime(res["dt"])),  # дата и время
+            'temp': res["main"]["temp"], #температура
+            'icon': res["weather"][0]["icon"],#код иконки погоды
+            'description': res['weather'][0]['description']# состояние погоды
+             }
         print("city :", city_info)
     except Exception as e:
-        print("Exception (find):", e)
+         print("Exception (find):", e)
 
-    # url = 'https://api.openweathermap.org/data/2.5/forecast?q={},RU&unit=metric&appid=' + API_key
-    #
-    # res = requests.get(url.format(city)).json()
-    # #выделяем необходимую информацию из url
-    # city_info = {
-    #     'city': city,
-    #     #'temp': str(res["main"]["temp"]),
-    #     #'info': res['weather']['icon']
-    # }
-    #
-    # context = {'info': city_info}
+    # контекст для передачи в шаблон
+    context = {'info': city_info}
     # #print(res.text)
 
-    return render(request, 'weather/index.html')
+    return render(request, 'weather/index.html', context)
