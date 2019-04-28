bcc14ef0d335172abe50611227c22d74e4f97fee
mongoy <mongoy@rambler.ru>
2019-04-28 22:16:26 +0900

    edit views html

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название города')
+    name_en = models.CharField(max_length=50, verbose_name='Название города анг.', default='-')
+    city_id = models.IntegerField(verbose_name='Индекс', default=2025339)
+    c_lon = models.FloatField(max_length=10, verbose_name='Долгота', default=113.5)
+    c_lat = models.FloatField(max_length=10, verbose_name='Широта', default=52.03)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
--- weather/views.py
+++ weather/views.py
@@ -2,29 +2,37 @@ import time
 import requests
 from django.shortcuts import render
 
+from weather.models import City
+
 
 def index(request):
     API_ID = 'b4ade3e9f47c260f170c8ee12da73bb0'  # ключ с сайта https://openweathermap.org
-    city = 'Чита'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={},ru&units=metric&APPID=' + API_ID
+    #city = 'Чита'
+    cityes = City.objects.all()#выбираем все объекты из базы
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={},ru&units=metric&lang=ru&APPID=' + API_ID
+    all_cityes = []
 
-    try:
+    for city in cityes:
 
-        res = requests.get(url.format(city)).json()# запрос на сервер получение JSON файла
-        # выделяем необходимую информацию из url
-        city_info = {
-            'city': city, #название города
-            'dt': time.strftime("%Y-%m-%d %H:%M:%S",time.gmtime(res["dt"])),  # дата и время
-            'temp': res["main"]["temp"], #температура
-            'icon': res["weather"][0]["icon"],#код иконки погоды
-            'description': res['weather'][0]['description']# состояние погоды
-             }
-        print("city :", city_info)
-    except Exception as e:
-         print("Exception (find):", e)
+        try:
+            res = requests.get(url.format(city.name_en)).json()# запрос на сервер получение JSON файла
+            # выделяем необходимую информацию из url
+            city_info = {
+                'city': city.name, #название города
+                'dt': time.strftime("%Y-%m-%d %H:%M:%S",time.gmtime(res["dt"])),  # дата и время
+                'temp': res["main"]["temp"], #температура
+                'icon': res["weather"][0]["icon"],#код иконки погоды
+                'description': res['weather'][0]['description']# состояние погоды
+                 }
+            #print("city :", city_info)
+        except Exception as e:
+             print("Exception (find):", e)
+
+        all_cityes.append(city_info)
+        print(all_cityes[0])
 
     # контекст для передачи в шаблон
-    context = {'info': city_info}
+    context = {'all_info': all_cityes}
     # #print(res.text)
 
     return render(request, 'weather/index.html', context)
